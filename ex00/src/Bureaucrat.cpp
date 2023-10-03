0c6e40a4c188135ae5d7da2ac860affa14a97a87
Eunsol Kim <eunskim@student.42heilbronn.de>
2023-10-03 18:48:00 +0200

    ex00

---
+++ ex00/src/Bureaucrat.cpp
@@ -0,0 +1,98 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eunskim <eunskim@student.42heilbronn.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/03 16:38:16 by eunskim           #+#    #+#             */
+/*   Updated: 2023/10/03 18:08:09 by eunskim          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+
+/* Orthodox Canonical Form */
+
+Bureaucrat::Bureaucrat(void) : _name("Defalt"), _grade(150)
+{
+	std::cout << PINK << "[Bureaucrat] Default constructor called" << RESET << std::endl;
+}
+
+Bureaucrat::Bureaucrat(std::string const &name, int grade) : _name(name)
+{
+	if (grade < 1)
+		throw GradeTooHighException();
+	else if (grade > 150)
+		throw GradeTooLowException();
+	else
+		_grade = grade;
+	std::cout << PINK << "[Bureaucrat] Name & grade constructor called" << RESET << std::endl;
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat &copy) : _name(copy._name), _grade(copy._grade)
+{
+	std::cout << PINK << "[Bureaucrat] Copy constructor called" << RESET << std::endl;
+}
+
+Bureaucrat &Bureaucrat::operator=(const Bureaucrat &op)
+{
+	std::cout << PINK << "[Bureaucrat] Copy assignment operator called" << RESET << std::endl;
+	if (this != &op)
+	{
+		_grade = op._grade;
+	}
+	return (*this);
+}
+
+Bureaucrat::~Bureaucrat(void)
+{
+	std::cout << PINK << "[Bureaucrat] Destructor called" << RESET << std::endl;
+}
+
+std::string const	&Bureaucrat::getName(void) const
+{
+	return (_name);
+}
+
+int	Bureaucrat::getGrade(void) const
+{
+	return (_grade);
+}
+
+void	Bureaucrat::setGrade(int grade)
+{
+	if (grade < 1)
+		throw GradeTooHighException();
+	else if (grade > 150)
+		throw GradeTooLowException();
+	else
+		_grade = grade;
+}
+
+void	Bureaucrat::incrementGrade(void)
+{
+	if (_grade == 1)
+		throw GradeTooHighException();
+	_grade -= 1;
+}
+
+void	Bureaucrat::decrementGrade(void)
+{
+	if (_grade == 150)
+		throw GradeTooLowException();
+	_grade += 1;
+}
+
+const char	*Bureaucrat::GradeTooLowException::what(void) const throw() {
+    return ("\x1B[38;2;224;224;224mGrade too low\x1B[0m");
+}
+
+const char	*Bureaucrat::GradeTooHighException::what(void) const throw() {
+    return ("\x1B[38;2;224;224;224mGrade too high\x1B[0m");
+}
+
+std::ostream	&operator<<(std::ostream &o, const Bureaucrat &bureaucrat)
+{
+	return (o << bureaucrat.getName() << ", bureaucrat grade " << bureaucrat.getGrade() << "." << std::endl);
+}
---
+++ ex00/src/main.cpp
@@ -0,0 +1,141 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eunskim <eunskim@student.42heilbronn.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/03 17:25:00 by eunskim           #+#    #+#             */
+/*   Updated: 2023/10/03 18:47:05 by eunskim          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+
+using std::cout;
+using std::endl;
+
+int main(void)
+{
+	try {
+		Bureaucrat	b("Bob", 100);
+
+		cout << b << endl;
+		b.incrementGrade();
+		cout << b << endl;
+		b.decrementGrade();
+		cout << b << endl;
+		b.incrementGrade();
+		cout << b << endl;
+	} catch (std::exception &e) {
+		cout << "Exception caught: " << e.what() << endl;
+	}
+	cout << endl;
+	try {
+		Bureaucrat	b("Thirsty", 1);
+
+		cout << b << endl;
+		b.decrementGrade();
+		cout << b << endl;
+		b.decrementGrade();
+		cout << b << endl;
+		b.incrementGrade();
+		cout << b << endl;
+	} catch (std::exception &e) {
+		cout << "Exception caught: " << e.what() << endl;
+	}
+	cout << endl;
+	try {
+		Bureaucrat	b("Yeyo", 1);
+
+		cout << b << endl;
+		b.incrementGrade();
+		cout << b << endl;
+		b.decrementGrade();
+		cout << b << endl;
+		b.incrementGrade();
+		cout << b << endl;
+	} catch (std::exception &e) {
+		cout << "Exception caught: " << e.what() << endl;
+	}
+	cout << endl;
+	try {
+        Bureaucrat b("Doll", 150);
+        
+        cout << b << endl;
+        b.incrementGrade();
+        cout << b << endl;
+        b.decrementGrade();
+        cout << b << endl;
+        b.incrementGrade();
+        cout << b << endl;
+    } catch (std::exception &e) {
+        cout << "Exception caught: " << e.what() << endl;
+    }
+	cout << endl;
+	try {
+        Bureaucrat b("Pippin", 150);
+        
+        cout << b << endl;
+        b.decrementGrade();
+        cout << b << endl;
+        b.incrementGrade();
+        cout << b << endl;
+        b.decrementGrade();
+        cout << b << endl;
+    } catch (std::exception &e) {
+        cout << "Exception caught: " << e.what() << endl;
+    }
+	cout << endl;
+	try {
+        Bureaucrat b("Nine", 151);
+        
+        cout << b << endl;
+    } catch (std::exception &e) {
+        cout << "Exception caught: " << e.what() << endl;
+    }
+	cout << endl;
+	try {
+        Bureaucrat b("Nine", 0);
+        
+        cout << b << endl;
+    } catch (std::exception &e) {
+        cout << "Exception caught: " << e.what() << endl;
+    }
+	cout << endl;
+	try {
+        Bureaucrat b("Nine", 15151);
+        
+        cout << b << endl;
+    } catch (std::exception &e) {
+        cout << "Exception caught: " << e.what() << endl;
+    }
+	cout << endl;
+	try {
+        Bureaucrat b("Nine", -100);
+        
+        cout << b << endl;
+    } catch (std::exception &e) {
+        cout << "Exception caught: " << e.what() << endl;
+    }
+	cout << endl;
+	try {
+        Bureaucrat b;
+        
+        cout << b << endl;
+		b.setGrade(-10);
+		cout << b << endl;
+    } catch (std::exception &e) {
+        cout << "Exception caught: " << e.what() << endl;
+    }
+	cout << endl;
+	try {
+        Bureaucrat b;
+        
+        cout << b << endl;
+		b.setGrade(200);
+		cout << b << endl;
+    } catch (std::exception &e) {
+        cout << "Exception caught: " << e.what() << endl;
+    }
+}
