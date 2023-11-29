d32a64dd89390956447cee2823bd187a48ed8c5e
Eunsol Kim <eunskim@student.42heilbronn.de>
2023-11-29 17:43:39 +0100

    setup for ex02

---
+++ cpp08/ex02/include/MutantStack.hpp
@@ -0,0 +1,23 @@
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+
+# include <stack>
+# include <iostream>
+
+template <typename T>
+class MutantStack
+{
+
+	private:
+		// pointer?
+
+	public:
+		// use of T in functions
+		void doSomething(const T& value)
+		{
+			// something
+		}
+
+};
+
+#endif
