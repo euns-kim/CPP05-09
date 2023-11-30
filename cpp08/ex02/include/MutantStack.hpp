fdf2f96fb03b90543784998a7250e0df31edd7cc
Eunsol Kim <eunskim@student.42heilbronn.de>
2023-11-30 16:17:47 +0100

    () added

--- cpp08/ex02/include/MutantStack.hpp
+++ cpp08/ex02/include/MutantStack.hpp
@@ -22,20 +22,20 @@ class MutantStack : public std::stack<T, Container>
 		virtual ~MutantStack(void) {}
 
 		typedef typename std::stack<T, Container>::container_type::iterator iterator;
-		iterator begin() { return this->c.begin(); }
-		iterator end() { return this->c.end(); }
+		iterator begin() { return (this->c.begin()); }
+		iterator end() { return (this->c.end()); }
 
 		typedef typename std::stack<T, Container>::container_type::reverse_iterator reverse_iterator;
-		reverse_iterator rbegin() { return this->c.rbegin(); }
-		reverse_iterator rend() { return this->c.rend(); }
+		reverse_iterator rbegin() { return (this->c.rbegin()); }
+		reverse_iterator rend() { return (this->c.rend()); }
 
 		typedef typename std::stack<T, Container>::container_type::const_iterator const_iterator;
-		const_iterator cbegin() { return this->c.cbegin(); }
-		const_iterator cend() { return this->c.cend(); }
+		const_iterator cbegin() { return (this->c.cbegin()); }
+		const_iterator cend() { return (this->c.cend()); }
 
 		typedef typename std::stack<T, Container>::container_type::const_reverse_iterator const_reverse_iterator;
-		const_reverse_iterator crbegin() { return this->c.crbegin(); }
-		const_reverse_iterator crend() { return this->c.crend(); }
+		const_reverse_iterator crbegin() { return (this->c.crbegin()); }
+		const_reverse_iterator crend() { return (this->c.crend()); }
 
 };
 
